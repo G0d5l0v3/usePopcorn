f3018e7c336b885beeb130f04b8ceeb641a1d8f3
Godslove Udo <udogodslove@gmail.com>
2024-03-05 13:58:05 +0100

    update

---
+++ src/components/header/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <NavLink>
+
+            </NavLink>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
